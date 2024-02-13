47183a96772f0afe8c8e73aa09fabb8da2a517d9
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-02-12 20:43:32 -0500

    client implementation for different values of n

--- src/main/java/edu/neu/coe/info6205/union_find/UnionFind_Client.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UnionFind_Client.java
@@ -6,7 +6,9 @@ public class UnionFind_Client {
 
     public static void main(String[] args){
         int n=1000;
-        System.out.println(count(n));
+        for(int i=1;i<6;i++) {
+            System.out.println(count(n*i));
+        }
 
     }
 
